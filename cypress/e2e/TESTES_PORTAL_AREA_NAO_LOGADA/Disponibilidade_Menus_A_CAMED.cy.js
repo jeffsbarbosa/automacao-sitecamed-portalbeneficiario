b788cd7b6e0246a27d8974304baa5d189bae24db
Jefferson Sousa Barbosa <jeffersonsb@camed.com.br>
2026-04-20 14:59:02 -0300

    ajuste do código yml

--- cypress/e2e/TESTES_PORTAL_AREA_NAO_LOGADA/Disponibilidade_Menus_A_CAMED.cy.js
+++ cypress/e2e/TESTES_PORTAL_AREA_NAO_LOGADA/Disponibilidade_Menus_A_CAMED.cy.js
@@ -7,7 +7,16 @@ describe("Testes de Disponibilidade – Menu A CAMED", () => {
  beforeEach(() => {
   cy.viewport(1280, 800)
   //cy.acessarHomeCamedSaude()
- cy.visit('https://www.camed.com.br')
+cy.intercept('POST', '**/google-analytics/**', { statusCode: 204 })
+cy.intercept('GET', '**/gtm.js**', { statusCode: 204 })
+cy.intercept('GET', '**/analytics**', { statusCode: 204 })
+
+ cy.visit('https://www.camed.com.br', {
+  timeout: 120000,
+  failOnStatusCode: false
+})
+
+cy.get('body', { timeout: 30000 }).should('be.visible')
 
 cy.contains('a', 'A Camed', { timeout: 30000 }).realHover()
 })
